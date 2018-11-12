332e0347fa21e54fd0152fecc2bbacc9b841963c
Mattie Terzolo <mattie.terzolo@gmail.com>
2018-11-12 14:28:20 -0800

    Add simple exponential smoothing model scripts

---
+++ bin/create_tables/create_simp_avg_table.py
@@ -0,0 +1,27 @@
+import psycopg2
+import yaml
+
+# Load config file
+with open("../../config.yml", 'r') as infile:
+    cfg = yaml.load(infile)
+
+# Connect to database
+conn = psycopg2.connect("host={} dbname={} user={} password={}".format(cfg['db_IP'],
+                                                                       cfg['db_name'],
+                                                                       cfg['db_user_name'],
+                                                                       cfg['db_password']))
+
+# Create table
+cur = conn.cursor()
+cur.execute("""
+DROP TABLE IF EXISTS model_simp_avg;
+
+CREATE TABLE model_simp_avg(
+
+    profile_name text,
+    mse float,
+    prediction float,
+    forecast_start timestamp
+);
+""")
+conn.commit()
